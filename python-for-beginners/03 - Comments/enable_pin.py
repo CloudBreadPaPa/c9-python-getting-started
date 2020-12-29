618b2c30433c8ddd71f9a3ebb7c8caf1ef77f9c1
Dae Woo Kim <daewoo.kim@live.com>
2020-12-30 00:28:26 +0900

    readme and comment code update

--- python-for-beginners/03 - Comments/enable_pin.py
+++ python-for-beginners/03 - Comments/enable_pin.py
@@ -1,16 +1,13 @@
-#The enable_pin method is not coded yet
-# I have created a dummy method so the code
-# will run without an error
-# Don't panic if you don't understand this part of the code
-# we cover methods in a separate module
+# enable_pin 메소드는 아직 코딩하지 않았습니다.
+# 더미 메서드를 만들었으므로 코드는 오류없이 실행됩니다.
+# 코드를 잘 이해하지 못하더라도 당황할 필요 없습니다. 별도의 모듈에서 메소드를 다룰 예정입니다.
+
 def enable_pin(user, pin):
     print('pin enabled')
 
-# Set current_user and pin to test values
+# current_user 및 pin 값을 설정하여 테스트 진행
 current_user = 'TEST123'
 pin = '123456'
 
-# Enable PIN check as listed in
-# security requirements
+# 보안 요구 사항에 나열된대로 PIN 체크를 활성화 하세요. - 코드를 잘 몰라도 놀랄 필요 없습니다.
 enable_pin(current_user, pin)
-
