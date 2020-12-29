f64e94151e969cef545e2b5bfc2ad8ba6f1719ca
dwkim <dwkim@microsoft.com>
2020-12-29 23:59:17 +0900

    ask_for_input.py update

--- python-for-beginners/02 - Print/ask_for_input.py
+++ python-for-beginners/02 - Print/ask_for_input.py
@@ -1,5 +1,10 @@
 # The input funciton allows you to prompt the user for a value
 # You need to declare a variable to hold the value entered by the user
+
+# input 함수를 사용하면 사용자가 값을 입력 할 수 있음.
+# 사용자가 입력 한 값을 유지하려면 변수를 선언해야함.
+
+
 name = input('What is your name? ')
 
 print(name)
