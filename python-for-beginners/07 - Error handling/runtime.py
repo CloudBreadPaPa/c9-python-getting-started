60b7ad1f8204ae50108834e577ea3f1c1ff76327
Dae Woo Kim <daewoo.kim@live.com>
2020-12-30 12:00:25 +0900

    error handling update

--- python-for-beginners/07 - Error handling/runtime.py
+++ python-for-beginners/07 - Error handling/runtime.py
@@ -3,7 +3,7 @@ y = 0
 try:
     print(x / y)
 except ZeroDivisionError as e:
-    # Optionally, log e somewhere
+    # 선택 항목으로, "e" 값을 어딘가에 로깅합니다.
     print('Sorry, something went wrong')
 except:
     print('Something really went wrong')
