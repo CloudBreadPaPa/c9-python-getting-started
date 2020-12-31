9b55fc5cc835b5a55d7da2ed2c1ebd7782371114
Dae Woo Kim <daewoo.kim@live.com>
2020-12-31 09:16:42 +0900

    formatting and linting update

--- more-python-for-beginners/01 - Formatting and linting/good.py
+++ more-python-for-beginners/01 - Formatting and linting/good.py
@@ -1,10 +1,11 @@
 def print_hello(name: str) -> str:
     """
     Greets the user by name
+	이름으로 사용자 환영
 
-	Parameters:
-		name (str): The name of the user
+	파라미터:
+		name (str): 사용자의 이름
 	Returns:
-		str: The greeting
+		str: 환영 메세지
 	"""
 	print('Hello, ' + name)
