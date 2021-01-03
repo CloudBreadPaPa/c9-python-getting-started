ff45e9e5f7c340b842e80aa7f54bc1dc571b3b29
Dae Woo Kim <daewoo.kim@live.com>
2021-01-03 16:50:39 +0900

    commnet error update

--- more-python-for-beginners/06 - Managing the file system/files.py
+++ more-python-for-beginners/06 - Managing the file system/files.py
@@ -9,7 +9,7 @@ print('\nfile name: ' + demo_file.name)
 # 파일의 확장자 가져오기
 print('\nfile suffix: ' + demo_file.suffix)
 
-# 폴도명 가져오기
+# 폴더명 가져오기
 print('\nfile folder: ' + demo_file.parent.name)
 
 # 크기 가져오기
